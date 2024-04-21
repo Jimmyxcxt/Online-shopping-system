c4eb1ff4af2d9c647956714d3fb5528d8cbe38f9
Jimmyxcxt <135585957+Jimmyxcxt@users.noreply.github.com>
2024-04-22 00:47:06 +0800

    Update AdminDao.java

--- dao/AdminDao.java
+++ dao/AdminDao.java
@@ -1,1 +1,12 @@
+package cn.itbaizhan.dao;
+import java.util.list;
+import cn.itbaizhan.po.Admin;
 
+public interface AdminDao{
+  public void save(Admin admin);
+  public void delete(Admin admin);
+  public Admin findAdminById(int id);
+  public List<Admin> findAllAdmins();
+  public void update(Admin admin);
+  public Admin getUserByLoginNameAndPassword(String name, Stringpasword);
+}
