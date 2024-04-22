1a354008d523916d122e2642e9b57e68aba8ccdf
Jimmyxcxt <135585957+Jimmyxcxt@users.noreply.github.com>
2024-04-23 01:18:27 +0800

    Update CommodityClassDao.java

--- dao/CommodityClassDao.java
+++ dao/CommodityClassDao.java
@@ -1,1 +1,11 @@
-
+package cn.itbaizhan.dao;
+import java.util.List;
+import cn.itbaizhan.po.CommodityClass;
+public interface CommodityClassDao{
+  public void save(CommodityClass commodityClass);
+  public void delete(CommodityClass id);
+  public CommodityClass findCommodityClassById(int id);
+  public List<CommodityClass> findAllCommodityClasses();
+  public void update(CommodityClass commodityClass);
+  public CommodityClass findCommodityClassByName(String name);
+}
