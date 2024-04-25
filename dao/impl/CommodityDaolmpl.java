03bfaa45c400cf4281bd5cc6e58c00f0c5b4372e
Jimmyxcxt <135585957+Jimmyxcxt@users.noreply.github.com>
2024-04-25 22:16:57 +0800

    Update CommodityDaolmpl.java

--- dao/impl/CommodityDaolmpl.java
+++ dao/impl/CommodityDaolmpl.java
@@ -5,10 +5,14 @@ import javax.annotation.Resource;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Component;
 
 import cn.itbaizhan.dao.CommodityDao;
 import cn.itbaizhan.po.Commodity;
 import cn.itbaizhan.po.CommodityClass;
+
 
 @Component("commodityDaoImpl")
 public class CommodityDaoImpl implements CommodityDao {
@@ -24,35 +28,28 @@ public class CommodityDaoImpl implements CommodityDao {
 		String hql = "from Commodity";
 		return (List<Commodity>)hibernateTemplate.find(hql);
 	}
-
 	@SuppressWarnings("unchecked")
 	public List<Commodity> findCommodityByName(String Name) {
 		System.out.println("商品名:"+Name);
 		String hql = "from Commodity c where c.commodityName like ?";
-		
 		return (List<Commodity>)hibernateTemplate.find(hql,"%"+Name+"%");
 	}
 	@SuppressWarnings("unchecked")
 	public List<Commodity> findCommodityByClass(CommodityClass commodityclass) {
 		System.out.println("种类:"+commodityclass);
 		String hql = "from Commodity c where c.commodityClass = ?";
-		
 		return (List<Commodity>)hibernateTemplate.find(hql,commodityclass);
 	}
-	
 	public Commodity findCommodityById(int id) {
 		Commodity commodity = (Commodity)hibernateTemplate.get(Commodity.class, id);
 		return commodity;
 	}
-
 	public void save(Commodity commodity) {
 		System.out.println(commodity);
 	 hibernateTemplate.save(commodity);
 	}
-
 	public void update(Commodity commodity) {
 	hibernateTemplate.update(commodity);
-
 	}
 	public HibernateTemplate getHibernateTmeplate() {
 		return hibernateTemplate;
@@ -67,5 +64,4 @@ public class CommodityDaoImpl implements CommodityDao {
 		
 		return (List<Commodity>)hibernateTemplate.find(hql,Name);
 	}
-	
 }
