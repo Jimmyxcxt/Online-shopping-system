de7eeb380ab3dc1c6cf5c8859713ee6e450100c2
Jimmyxcxt <135585957+Jimmyxcxt@users.noreply.github.com>
2024-04-25 15:17:14 +0800

    Update MessageDao.java

--- dao/MessageDao.java
+++ dao/MessageDao.java
@@ -1,1 +1,11 @@
-
+package cn.itbaizhan.dao;
+import java.util.Date;
+import java.util.List;
+import cn.itbaizhan.po.Message;
+public interface MessageDao{
+  public void save(Message message);
+  public void delete(Message message);
+  public Message findMessageById(int id);
+  public List<Message> findAllMessage();
+  public MessagequeryMessage(String username);
+}
