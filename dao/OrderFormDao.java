ad7247f74bafd4aabd575a7ad2ab012bf06f1312
Jimmyxcxt <135585957+Jimmyxcxt@users.noreply.github.com>
2024-04-25 15:31:58 +0800

    Update OrderFormDao.java

--- dao/OrderFormDao.java
+++ dao/OrderFormDao.java
@@ -1,1 +1,16 @@
+package cn.itbaizhan.dao;
+import java.util.List;
+import cn.itbaizhan.po.OrderForm;
+import cn.itbaizhan.po.User;
+
+public interface OrderFormDao {
+	public void save(OrderForm orderForm);
+	public void delete(OrderForm orderForm);
+	public OrderForm findOrderFormById(int id);
+	public List<OrderForm> findAllOrderForms();
+	public void update(OrderForm orderForm);
+	public OrderForm queryOrderForm(int orderFormId);
+	public List<OrderForm> findOrderFormByUserName(String username);
+	
+}
 
